7dc7cb1030dd8abc68c702b17e6b5e43d49c626c
Takuya Sugawara <suga@Takuyas-MacBook-Air.local>
2018-01-01 20:49:25 +0900

    save temporary

--- main.go
+++ main.go
@@ -6,12 +6,44 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
-type Result struct {
-	RenderedBody String
-	Id           int `json:"id"`
-	LikesCount   int `json:"likes_count"`
+type Result []struct {
+	RenderedBody   string      `json:"rendered_body"`
+	Body           string      `json:"body"`
+	Coediting      bool        `json:"coediting"`
+	CommentsCount  int         `json:"comments_count"`
+	CreatedAt      time.Time   `json:"created_at"`
+	Group          interface{} `json:"group"`
+	ID             string      `json:"id"`
+	LikesCount     int         `json:"likes_count"`
+	Private        bool        `json:"private"`
+	ReactionsCount int         `json:"reactions_count"`
+	Tags           []struct {
+		Name     string        `json:"name"`
+		Versions []interface{} `json:"versions"`
+	} `json:"tags"`
+	Title     string    `json:"title"`
+	UpdatedAt time.Time `json:"updated_at"`
+	URL       string    `json:"url"`
+	User      struct {
+		Description       string      `json:"description"`
+		FacebookID        string      `json:"facebook_id"`
+		FolloweesCount    int         `json:"followees_count"`
+		FollowersCount    int         `json:"followers_count"`
+		GithubLoginName   string      `json:"github_login_name"`
+		ID                string      `json:"id"`
+		ItemsCount        int         `json:"items_count"`
+		LinkedinID        string      `json:"linkedin_id"`
+		Location          string      `json:"location"`
+		Name              string      `json:"name"`
+		Organization      string      `json:"organization"`
+		PermanentID       int         `json:"permanent_id"`
+		ProfileImageURL   string      `json:"profile_image_url"`
+		TwitterScreenName interface{} `json:"twitter_screen_name"`
+		WebsiteURL        string      `json:"website_url"`
+	} `json:"user"`
 }
 
 func main() {
