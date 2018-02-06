2c4315fbd05c62edbde7c47a0e96fd9a22a9ddfa
Takuya Sugawara <suga@Takuyas-MacBook-Air.local>
2018-02-07 08:05:02 +0900

    処理完成。lambda対応を残すのみ

--- main.go
+++ main.go
@@ -1,59 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	//	"github.com/aws/aws-lambda-go/lambda"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 type Result struct {
-	RenderedBody   string      `json:"rendered_body"`
-	Body           string      `json:"body"`
-	Coediting      bool        `json:"coediting"`
-	CommentsCount  int         `json:"comments_count"`
-	CreatedAt      time.Time   `json:"created_at"`
-	Group          interface{} `json:"group"`
-	ID             string      `json:"id"`
-	LikesCount     int         `json:"likes_count"`
-	Private        bool        `json:"private"`
-	ReactionsCount int         `json:"reactions_count"`
-	Tags           []struct {
-		Name     string        `json:"name"`
-		Versions []interface{} `json:"versions"`
-	} `json:"tags"`
-	Title     string    `json:"title"`
-	UpdatedAt time.Time `json:"updated_at"`
-	URL       string    `json:"url"`
-	User      struct {
-		Description       string      `json:"description"`
-		FacebookID        string      `json:"facebook_id"`
-		FolloweesCount    int         `json:"followees_count"`
-		FollowersCount    int         `json:"followers_count"`
-		GithubLoginName   string      `json:"github_login_name"`
-		ID                string      `json:"id"`
-		ItemsCount        int         `json:"items_count"`
-		LinkedinID        string      `json:"linkedin_id"`
-		Location          string      `json:"location"`
-		Name              string      `json:"name"`
-		Organization      string      `json:"organization"`
-		PermanentID       int         `json:"permanent_id"`
-		ProfileImageURL   string      `json:"profile_image_url"`
-		TwitterScreenName interface{} `json:"twitter_screen_name"`
-		WebsiteURL        string      `json:"website_url"`
-	} `json:"user"`
+	CreatedAt  time.Time `json:"created_at"`
+	LikesCount int       `json:"likes_count"`
+	Title      string    `json:"title"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	URL        string    `json:"url"`
 }
 
 func main() {
-	var userId string = `sivertigo`
-	url := "http://qiita.com/api/v2/users/" + userId + "/items"
+	//	lambda.Start(getLikesCount)
+	getLikesCount()
+}
+
+func getLike(userId string) int {
+	myurl := "http://qiita.com/api/v2/users/" + userId + "/items"
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", myurl, nil)
 	if err != nil {
 		log.Fatal(err)
-
 	}
 
 	req.Header.Add("x-auth-token", "token1")
@@ -78,14 +55,45 @@ func main() {
 	err = json.Unmarshal([]byte(body), &results)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	var totalLikes int
-	fmt.Println("ユーザー名" + userId)
+	fmt.Print("ユーザー名" + userId + " ")
 	for i := 0; i < len(results); i++ {
-		fmt.Println(results[i].Title)
-		fmt.Println(results[i].LikesCount)
-		totalLikes += results[i].LikesCount
+		if results[i].CreatedAt.Sub(time.Date(2018, time.January, 0, 0, 0, 0, 0, time.UTC)) > 0 {
+			totalLikes += results[i].LikesCount
+		}
 	}
-	fmt.Println(totalLikes)
+	fmt.Print(totalLikes)
+	return totalLikes
+}
+
+func postLike(userId string, slackUrl string, total int) error {
+	myUrl := "https://qiita.com/" + userId
+	jsonStr := `{"text":"` + userId + `さんの2018年に書いた記事のいいね数は合計 ` + strconv.Itoa(total) + `です. URL: ` + myUrl + `"}`
+	req, err := http.NewRequest(
+		"POST",
+		slackUrl,
+		bytes.NewBuffer([]byte(jsonStr)),
+	)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return err
+}
+
+func getLikesCount() {
+	userId := []string{"sivertigo", "sadayuki-matsuno"}
+	slackUrl := "https://hooks.slack.com/services/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"
+	for i := 0; i < len(userId); i++ {
+		postLike(userId[i], slackUrl, getLike(userId[i]))
+	}
 }
